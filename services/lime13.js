2a47eff0b97d917018a0832972e5352d8371ce6b
Yunjia Li <yl2@ecs.sotonacuk>
2013-04-13 22:05:47 +0100

    update lime13 services

--- services/lime13.js
+++ services/lime13.js
@@ -111,53 +111,53 @@ exports.mfStat = function(callback){
 	var dir = "tests/dailymotion/nerdjson/";
 	var csv = fs.createWriteStream('tests/lime13/mf.csv', {'flags': 'a'});
 	var contentHeader = "videoid"+csvSeparator+"idEntity"+csvSeparator+"label"+csvSeparator+"startChar"+csvSeparator+"endChar"+csvSeparator+
-			"extractorType"+csvSeparator+"nerdType"+csvSeparator+"url"+csvSeparator+"confidence"+csvSeparator+
+			"extractorType"+csvSeparator+"nerdType"+csvSeparator+"uri"+csvSeparator+"confidence"+csvSeparator+
 			"relevance"+csvSeparator+"extractor"+csvSeparator+"startNPT"+csvSeparator+"endNPT\r\n";
 	csv.write(contentHeader);
-	
+	var len=0;
 	var q = async.queue(function (task, callback) {
-    	fs.readFile(dir+task.filename,'utf-8',function(err,text){
-    	    console.log(task.filename);
-        	if (err) 
-				log.debug(err);
-        	//var result = JSON.parse(text);
-        	eval("var result = "+text); //sometimes there the json includes some special characters that will lead to error
-        	if(result !== undefined)
-        	{
-        		for(var i=0;i<result.length;i++)
-        		{
-        			var content = "";
-        			content+=task.filename.replace(".json","")+csvSeparator; //write the video id
-        			content+=result[i].idEntity !== undefined?result[i].idEntity:"null";
-        			content+=csvSeparator;
-        			content+=result[i].label!== undefined?result[i].label.replace(/(\r\n|\n|\r)/gm," "):"null";
-        			content+=csvSeparator;
-        			content+=result[i].startChar!== undefined?result[i].startChar:"null";
-        			content+=csvSeparator;
-        			content+=result[i].endChar!== undefined?result[i].endChar:"null";
-        			content+=csvSeparator;
-        			content+=result[i].extractorType!== undefined?result[i].extractorType:"null";
-        			content+=csvSeparator;
-        			content+=result[i].nerdType!== undefined?result[i].nerdType:"null";
-        			content+=csvSeparator;
-        			content+=result[i].url!== undefined?result[i].url:"null";
-        			content+=csvSeparator;
-        			content+=result[i].confidence!== undefined?result[i].confidence:"null";
-        			content+=csvSeparator;
-        			content+=result[i].relevance!== undefined?result[i].relevance :"null";
-        			content+=csvSeparator;
-        			content+=result[i].extractor!== undefined?result[i].extractor:"null";
-        			content+=csvSeparator;
-        			content+=result[i].startNPT!== undefined?result[i].startNPT :"null";
-        			content+=csvSeparator;
-        			content+=result[i].endNPT!== undefined?result[i].endNPT:"null";
-        			content+="\r\n";
-        			csv.write(content);
-        		}
-        	}
-    	});
-	   callback();
-	}, 10);
+    	var text = fs.readFileSync(dir+task.filename,'utf-8');
+	    log.debug(task.filename);
+    	//if (err) 
+			//log.debug(err);
+    	eval("var result = "+text); //sometimes there the json includes some special characters that will lead to error
+    	if(result !== undefined)
+    	{
+    		len += result.length;
+    		for(var i=0;i<result.length;i++)
+    		{
+    			var content = "";
+    			content+=task.filename.replace(".json","")+csvSeparator; //write the video id
+    			content+=result[i].idEntity !== undefined?result[i].idEntity:"null";
+    			content+=csvSeparator;
+    			content+=result[i].label!== undefined?result[i].label.replace(/(\r\n|\n|\r)/gm," "):"null";
+    			content+=csvSeparator;
+    			content+=result[i].startChar!== undefined?result[i].startChar:"null";
+    			content+=csvSeparator;
+    			content+=result[i].endChar!== undefined?result[i].endChar:"null";
+    			content+=csvSeparator;
+    			content+=result[i].extractorType!== undefined?result[i].extractorType:"null";
+    			content+=csvSeparator;
+    			content+=result[i].nerdType!== undefined?result[i].nerdType:"null";
+    			content+=csvSeparator;
+    			content+=result[i].uri!== undefined?result[i].uri:"null";
+    			content+=csvSeparator;
+    			content+=result[i].confidence!== undefined?result[i].confidence:"null";
+    			content+=csvSeparator;
+    			content+=result[i].relevance!== undefined?result[i].relevance :"null";
+    			content+=csvSeparator;
+    			content+=result[i].extractor!== undefined?result[i].extractor:"null";
+    			content+=csvSeparator;
+    			content+=result[i].startNPT!== undefined?result[i].startNPT :"null";
+    			content+=csvSeparator;
+    			content+=result[i].endNPT!== undefined?result[i].endNPT:"null";
+    			content+="\r\n";
+    			csv.write(content);
+    		}
+    	}
+    	log.debug("len:"+len);
+	   	callback();
+	}, 1); 
 	
 	fs.readdir(dir,function(err,files){
     	if (err) 
@@ -169,7 +169,9 @@ exports.mfStat = function(callback){
     		q.push({filename:files[j]},function(err){
     			//log.debug("Reading file "+files[j]);
     			if(err!= null)
+    			{
     				log.debug("ERROR!"+err);
+    			}
     		});
     	}
 	});
