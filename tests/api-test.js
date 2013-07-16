7da8c20ef266e2342b2fe219955e0f9c804fff38
Yunjia Li <yl2@ecs.sotonacuk>
2013-07-16 11:49:32 +0100

    Add test case for static thumbnail delivery

--- tests/api-test.js
+++ tests/api-test.js
@@ -24,10 +24,40 @@ var client = restify.createJsonClient({
  //   done();
 //});
 
+/**
+ * Static files test
+ */
+describe('static_thumbnail',function(){
+	it("Serve a specific thumbnail file",function(done){
+        client.get('/thumbnail/apitest/2.7.jpg', function(err, req, res, data) {
+            if (err) {
+                should.not.exist(err);
+            }
+            else {
+                res.statusCode.should.equal(200);
+                done();
+            }
+        });
+	});
+	
+	it("Search thumbnail file given start and end time",function(done){
+        client.get('/thumbnail?id=apitest&start=5000&end=7000', function(err, req, res, data) {
+            if (err) {
+                should.not.exist(err);
+            }
+            else {
+                res.statusCode.should.equal(200);
+                done();
+            }
+        });
+	});
+});
+
+
 
 if(config.api.generateThumbnail == true)
 {
-	describe('API generateThumbnail mp4',function(){
+	describe('api_generateThumbnail_mp4',function(){
 		it("Generate thumbnail pictures for a given mp4 file",function(done){
 			var videourl = encodeURIComponent("http://synote.org/resource/yunjiali/bigbuck.mp4");
             client.get('/api/generateThumbnail?videourl='+videourl+'&id=1234567890mp4', function(err, req, res, data) {
@@ -43,7 +73,7 @@ if(config.api.generateThumbnail == true)
 		});
 	});
 	
-	describe('API generateThumbnail YouTube',function(){
+	describe('api_generateThumbnail_yt',function(){
 		it("Generate thumbnail pictures for a YouTube video",function(done){
 			var videourl = "http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DWkQjYHx3NY8";
             client.get('/api/generateThumbnail?videourl='+videourl+'&id=098765431youtube', function(err, req, res, data) {
@@ -58,7 +88,7 @@ if(config.api.generateThumbnail == true)
 		});
 	});
 	
-	describe('API generateThumbnail Dailymotion',function(){
+	describe('api_generateThumbnail_dm',function(){
 		it("Generate thumbnail pictures for a Dailymotion video",function(done){
 			var videourl = encodeURIComponent("http://www.dailymotion.com/video/x111zzq_snorting-souls-is-hard-to-do-the-gate_fun");
             client.get('/api/generateThumbnail?videourl='+videourl+'&id=098765431dailymotion', function(err, req, res, data) {
@@ -76,7 +106,7 @@ if(config.api.generateThumbnail == true)
 
 if(config.api.getMetadata == true)
 {
-	describe('getMetadata',function(){
+	describe('api_getMetadata_yt',function(){
 		it("should get YouTube video metadata",function(done){
 			var videourl =encodeURIComponent("http://www.youtube.com/watch?v=WkQjYHx3NY8");
             client.get('/api/getMetadata?videourl='+videourl,function(err, req, res, data) {
